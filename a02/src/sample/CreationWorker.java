f50852acd7e7c2e27e298d564164aa0fa04ec76b
Charles <cpat430@aucklanduni.ac.nz>
2019-09-27 20:41:06 +1200

    Made a minor change to FlickrImageExtractor to return number of images found and got the image/video creation to work. Still need to sort the superimposed text and merging of audio.

--- a02/src/sample/CreationWorker.java
+++ a02/src/sample/CreationWorker.java
@@ -47,9 +47,35 @@ public class CreationWorker extends Task<String> {
                 message = "Error";
             } else if (imagesFound == 0) {
                 message = "No images found";
-            } else {
-                double duration = _audio.mergeAudio(_creationDir);
+            } else { // create here?
+                double duration = _audio.mergeAudio(_creationDir); // merges audio
+                System.out.println(duration);
+                System.out.println(imagesFound);
                 message = "Success";
+
+                // set the creation name with creationName_duration
+                String creationName = _creationDir.getName();
+
+                // resize all the images to the same size 1920x1080
+                String command = "mogrify -resize 1920x1080 ./src/creations/" + creationName + "/*.jpg"; // may need to specify this is used in brief
+                System.out.println(command);
+                Terminal.command(command);
+
+                // merge the images
+                command = "cat ./src/creations/" + creationName + "/*.jpg | ffmpeg -f image2pipe -r " + ((1.0 / duration) * imagesFound) + " -vcodec mjpeg -i - -vcodec libx264 -vf scale=1920:-2 -vf scale=-2:1080 ./src/creations/" + creationName + "/" + creationName + "Temp.mp4";
+                System.out.println(command);
+                Terminal.command(command);
+
+                // add the name onto the video
+//                command = "ffmpeg -i ./src/creations/" + creationName + "/" + creationName + "Temp.mp4 -vf drawtext=\"" +
+//                        "text='" + creationName + "': fontcolor=white: fontsize=24: box=1: boxcolor=black@0.5: " +
+//                        "boxborderw=5: -codec:a copy ./src/creations/" + creationName + "/" + creationName + ".mp4";
+//                System.out.println(command);
+//                Terminal.command(command);
+
+                // merge the video and images
+//                command = "ffmpeg -i ./src/creations/" + creationName + ".mp4 -i ./src/creations/output.wav -c:v copy -c:a aac -strict experimental ./src/creations/" + creationName + ".mp4 &> /dev/null";
+//                Terminal.command(command);
             }
             return message;
         }
